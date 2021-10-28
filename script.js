70da3f41086fbf07d28dfe20a4f5225a43eec4f6
Sakshi Chede <78843328+Sakiii8499@users.noreply.github.com>
2021-10-28 19:50:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function myFunction1(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("snacks-content").style.display="block";
+}
+function myFunction2(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("sweets-content").style.display="block";
+}
+function myFunction3(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("south-content").style.display="block";
+}
+function myFunction4(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("italian-content").style.display="block";
+}
+function myFunction5(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("bakery-content").style.display="block";
+}
+function myFunction6(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("icecream-content").style.display="block";
+}
+function myFunction7(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("dairy-content").style.display="block";
+}
+function myFunction8(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("breakfast-content").style.display="block";
+}
+function myFunction9(){
+    document.getElementById("main-content").style.display="none";
+    document.getElementById("beverages-content").style.display="block";
+}
+
+// Methods
+
+function Function1(){
+    document.getElementById("snacks").style.display="none";
+    document.getElementById("chips").style.display="block";
+}
+function Function2(){
+    document.getElementById("snacks").style.display="none";
+    document.getElementById("samosa").style.display="block";
+}
+function Function3(){
+    document.getElementById("snacks").style.display="none";
+    document.getElementById("shegaon").style.display="block";
+}
